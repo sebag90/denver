26c230d983f5d4d2883d0ed3e6ace791a525f890
seba <seba.g90@gmail.com>
2024-11-01 16:01:14 +0100

    rename init --> create add list

--- denver/bin/cli.py
+++ denver/bin/cli.py
@@ -7,9 +7,11 @@ def get_args():
     )
     subparsers = parser.add_subparsers(dest="subparser")
 
-    init = subparsers.add_parser("init", help="create a new environment")
-    init.add_argument("name")
-    init.add_argument("--version", "-v", default="3.12", type=str)
+    create = subparsers.add_parser("create", help="create a new environment")
+    create.add_argument("name")
+    create.add_argument("--version", "-v", default="3.12", type=str)
+
+    list_envs = subparsers.add_parser("list")
 
     activate = subparsers.add_parser("activate")
     activate.add_argument("name")
---
+++ denver/src/list.py
@@ -0,0 +1,7 @@
+from .utils import get_env_base_dir
+
+def main(args):
+    denver_base_config = get_env_base_dir()
+    for env_dir in denver_base_config.iterdir():
+        if env_dir.is_dir():
+            print(f"* {env_dir.stem}")
