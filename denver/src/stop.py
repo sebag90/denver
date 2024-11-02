737e638a3690113691955d6ee71ec6924ed83faa
seba <seba.g90@gmail.com>
2024-11-02 15:42:04 +0100

    check existance before stopping

--- denver/src/stop.py
+++ denver/src/stop.py
@@ -1,6 +1,13 @@
-from .utils import docker_compose
+from pathlib import Path
+
+from .utils import docker_compose, get_env_base_dir
 
 
 def main(args):
+    denver_base_dir = get_env_base_dir()
+    if not Path(f"{denver_base_dir}/{args.name}").exists():
+        print(f"Environment {args.name} is missing, create it first")
+        return
+
     docker_compose(args.name, "down")
     print(f"Environment {args.name} stopped")
