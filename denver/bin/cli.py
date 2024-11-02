a3d2be0e35d4ee9cf1bb9a00430f4e238cfe2265
seba <seba.g90@gmail.com>
2024-11-02 12:28:04 +0100

    modify from stdin

--- denver/bin/cli.py
+++ denver/bin/cli.py
@@ -7,6 +7,11 @@ def get_args():
     )
     subparsers = parser.add_subparsers(dest="subparser")
 
+    # SIMPLE SUBPARSERS
+    subparsers.add_parser("list", help="list all available environments")
+    subparsers.add_parser("prune", help="remove all environments")
+
+    # CREATE ENV
     create = subparsers.add_parser("create", help="create a new environment")
     create.add_argument("name")
     create.add_argument("--version", "-v", default="3.12", type=str)
@@ -14,23 +19,25 @@ def get_args():
         "--root", action="store_true", help="the user inside the container will be root"
     )
 
-    subparsers.add_parser("list", help="list all available environments")
-    subparsers.add_parser("prune", help="remove all environments")
-
+    # REBUILD
     rebuild = subparsers.add_parser("rebuild", help="rebuild an environment from image")
     rebuild.add_argument("name")
 
+    # ACTIVATE
     activate = subparsers.add_parser(
         "activate", help="activate the shell of an environment"
     )
     activate.add_argument("name")
 
+    # REMOVE
     remove = subparsers.add_parser("remove", help="remove an environment")
     remove.add_argument("name")
 
+    # STOP
     stop = subparsers.add_parser("stop", help="stop the container of an environment")
     stop.add_argument("name")
 
+    # CONFIG
     config = subparsers.add_parser(
         "config", help="modify the configuration of an environment"
     )
@@ -41,6 +48,11 @@ def get_args():
     )
     config.add_argument(
         "-e", "--environment", help="the environment to modify", required=True
+    )
+    config.add_argument(
+        "--from-stdin",
+        action="store_true",
+        help="Redirect stdin into the file you want to modify",
     )
 
     args = parser.parse_args()
--- denver/src/config.py
+++ denver/src/config.py
@@ -1,12 +1,24 @@
+from io import TextIOWrapper
 import os
 from pathlib import Path
 import subprocess
+import sys
 
 from .utils import get_env_base_dir
 
 
 def main(args):
-    editor = os.getenv("EDITOR")
     denver_base_dir = get_env_base_dir()
-    file = Path(f"{denver_base_dir}/{args.environment}/{args.file}")
-    subprocess.run([editor, file])
+    if not Path(f"{denver_base_dir}/{args.environment}").exists():
+        print(f"Environment {args.environment} is missing, create it first")
+        return
+
+    file_to_modify = Path(f"{denver_base_dir}/{args.environment}/{args.file}")
+
+    if args.from_stdin is True:
+        input_stream = TextIOWrapper(sys.stdin.buffer, encoding="utf-8")
+        file_to_modify.write_text(input_stream.read(), encoding="utf-8")
+        return
+
+    editor = os.getenv("EDITOR")
+    subprocess.run([editor, file_to_modify])
