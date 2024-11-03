46974c5c9dd550724897814441fc7d8d4208d396
seba <seba.g90@gmail.com>
2024-11-03 17:47:23 +0100

    check if there are environments before pruning

--- denver/src/prune.py
+++ denver/src/prune.py
@@ -2,6 +2,12 @@ from .utils import remove_env, get_env_base_dir
 
 
 def main(args):
+    envs = [file.stem for file in get_env_base_dir().iterdir()]
+
+    if len(envs) == 0:
+        print("Nothing to do here")
+        return
+
     user_input = input("Are you sure you want to delete all environments [y/n]\n> ")
     if user_input.lower().strip() != "y":
         return
