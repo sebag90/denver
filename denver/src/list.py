341a1f8703c2c036f8fc7ab382d780a7f306f55f
sebag90 <seba.g90@gmail.com>
2024-11-09 17:31:24 +0100

    prevent error if base dir was not created yet

--- denver/src/list.py
+++ denver/src/list.py
@@ -2,8 +2,11 @@ from .utils import get_env_base_dir
 
 
 def main(args):
-    denver_base_config = get_env_base_dir()
-    for env_dir in denver_base_config.iterdir():
+    denver_base_dir = get_env_base_dir()
+    if not denver_base_dir.exists():
+        return 0
+    
+    for env_dir in denver_base_dir.iterdir():
         if env_dir.is_dir():
             print(f"* {env_dir.stem}")
 
--- setup.py
+++ setup.py
@@ -2,41 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="denver",
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version="1.0.4",
+    version="1.0.5",
     license="MIT",
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
     packages=find_packages(),
     include_package_data=True,
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    # install_requires=["tensorflow==1.11.0","tensorflow-gpu==1.11.0", "pillow==5.4.1", "numpy"],
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    # extras_require={
-    #    'dev': ['check-manifest'],
-    #    'test': ['coverage'],
-    # },
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
     package_data={"": ["template/*", "config.toml"]},
-    # data_files=[('project/configuration', ['project/configuration/configuration.ini.template'])],
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[('my_data', ['data/data_file.txt'])],
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
     entry_points={
         "console_scripts": ["denver = denver.bin.run:main"],
     },
