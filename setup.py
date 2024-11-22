3b2a2a1e4480b89753a95aed8f7d4ea1336c7d87
sebag90 <seba.g90@gmail.com>
2024-11-23 00:39:37 +0100

    more requirements and version to external files

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
+from pathlib import Path
 from setuptools import setup, find_packages
+
+
+requirements = Path("requirements.txt").read_text(encoding="utf-8").split("\n")
+version = Path("denver/__version__").read_text()
+
 
 setup(
     name="denver",
-    version="0.6.0",
+    version=version,
     license="MIT",
     packages=find_packages(),
-    install_requires=["pick"],
+    install_requires=requirements,
     include_package_data=True,
     package_data={"": ["template/*", "config.toml"]},
     entry_points={
