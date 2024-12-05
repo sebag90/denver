57fa85fb8046acf588ccbaf46211349cf8733c7c
sebag90 <seba.g90@gmail.com>
2024-12-05 22:51:11 +0100

    fix bug with old imports

--- denver/src/stop.py
+++ denver/src/stop.py
@@ -1,11 +1,10 @@
 from pathlib import Path
 
-from .utils import docker_compose, get_env_base_dir
+from .utils import Config, docker_compose
 
 
 def main(args):
-    denver_base_dir = get_env_base_dir()
-    if not Path(f"{denver_base_dir}/{args.name}").exists():
+    if not Path(f"{Config.paths.base_dir}/{args.name}").exists():
         print(f"Environment {args.name} is missing, create it first")
         return 1
 
